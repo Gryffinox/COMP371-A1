a337a6c546b1a8d8c7bb8be478eac35a15fe1728
amanda6mard <61531228+amanda6mard@users.noreply.github.com>
2021-07-20 15:10:04 -0400

    refactored colors.h

--- COMP371-A1/colors.h
+++ COMP371-A1/colors.h
@@ -9,19 +9,20 @@
 #ifndef colors_h
 #define colors_h
 
-float rgbValue(float c);
+glm::vec3 rgb(float r, float g, float b);
 
 const glm::vec3 WHITE = glm::vec3(1.0f, 1.0f, 1.0f);
-const glm::vec3 DARK_BLUE = glm::vec3(rgbValue(38), rgbValue(70), rgbValue(83));
-const glm::vec3 YELLOW = glm::vec3(rgbValue(233), rgbValue(196), rgbValue(106));
-const glm::vec3 ORANGE = glm::vec3(rgbValue(244), rgbValue(162), rgbValue(97));
-const glm::vec3 BLUE = glm::vec3(rgbValue(0), rgbValue(0), rgbValue(255));
-const glm::vec3 RED = glm::vec3(rgbValue(255), rgbValue(0), rgbValue(0));
-const glm::vec3 GREEN = glm::vec3(rgbValue(0), rgbValue(255), rgbValue(0));
+const glm::vec3 BLACK = glm::vec3(1.0f, 1.0f, 1.0f);
+const glm::vec3 DARK_BLUE = rgb(38,70,83);
+const glm::vec3 YELLOW = rgb(233,196,106);
+const glm::vec3 ORANGE = rgb(244,162,97);
+const glm::vec3 BLUE = rgb(0,0,255);
+const glm::vec3 RED = rgb(255,0,0);
+const glm::vec3 GREEN = rgb(0,255,0);
 
-float rgbValue(float c)
+glm::vec3 rgb(float r, float g, float b)
 {
-    return c/255.0f;
+    return glm::vec3(r/255,g/255,b/255);
 }
 
 
