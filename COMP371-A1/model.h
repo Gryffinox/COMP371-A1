dee92469f3ad369649e61ef495615eb61bcc6f4d
Calvin <calvinkylee78@gmail.com>
2021-07-20 16:49:02 -0400

    Model rotation
    Added functionality for models to rotate. Split the model coordinate displacement and actual translation vectors into separate matrices. Adjusted worldMatrix multiplication.

--- COMP371-A1/model.h
+++ COMP371-A1/model.h
@@ -20,7 +20,7 @@ public:
     glm::vec3* colors;
     glm::vec3 center;
     float front;
-    float numCubes;
+    int numCubes;
     bool diffColors;
     bool unshuffle;                     //Determines if the model is currently moving towards the shuffled shape or the regular shape
     
@@ -29,7 +29,7 @@ public:
         positionsDefault = new glm::vec3;
         colors = new glm::vec3;
         front = 0.0f;
-        numCubes = 0.0f;
+        numCubes = 0;
         diffColors = false;
         unshuffle = false;
     }
@@ -39,7 +39,7 @@ public:
         colors = new glm::vec3;
         diffColors = false;
         //Dynmically set array size for new model
-        numCubes = size;
+        numCubes = (int)size;
         positions = new glm::vec3[numCubes];
         positionsDefault = new glm::vec3[numCubes];
         //copy inputs into the objects parameters
@@ -90,17 +90,21 @@ public:
             positions[i] -= center;
             positionsDefault[i] -= center;
         }
-        
+
+        //Flag for shuffle later
         unshuffle = false;
     }
     
     //Shuffle function which "flattens" the model
     void shuffle(float deltaTime)
     {
+        //all cubes at front and shuffled on to each other
         bool allFront = true;
+        //all cubes returned to original position?
         bool allDefault = true;
         for (int i = 0; i<numCubes; i++)
         {
+            //Move all the cubes back to their original positions
             if(unshuffle)
             {
                 allFront = false;
@@ -110,6 +114,7 @@ public:
                     allDefault = false;
                 }
             }
+            //We're shuffling all the cubes on to each other
             else
             {
                 if (positions[i].z < front)
@@ -120,30 +125,38 @@ public:
                 }
             }
         }
+        //if cubes in default position, start shuffling them
         if (allDefault)
         {
             unshuffle = false;
         }
+        //if all cubes reached the front, toggle flag to move them back
         if (allFront)
         {
             unshuffle = true;
         }
     }
     
-    void draw(int worldLoc, int startIndex, float scale, glm::vec3 coordinates, glm::vec3 angles) //add angle
+    //Draw the model
+    //Pass in each transformation as a set of coordinates except for scale which applies to all dimensions
+    void draw(int worldLoc, int startIndex, float scale, glm::vec3 coordinates, glm::vec3 angles)
     {
         for(int i = 0; i<numCubes; i++)
         {
             //set each transformation matrix based on input variables
             glm::mat4 scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, scale));
-            glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), (coordinates + scale*(positions[i])));
+            //glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), coordinates + scale*(positions[i]));
+            glm::mat4 positionMatrix = glm::translate(glm::mat4(1.0f), scale * (positions[i]));
+            glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), coordinates);
             glm::mat4 rotationMatrix = 
                 glm::rotate(glm::mat4(1.0f), glm::radians(angles.x), glm::vec3(1.f, .0f, .0f)) * 
                 glm::rotate(glm::mat4(1.0f), glm::radians(angles.y), glm::vec3(.0f, 1.f, .0f)) *
                 glm::rotate(glm::mat4(1.0f), glm::radians(angles.z), glm::vec3(.0f, .0f, 1.f)
             );
             //calculate final transformation
-            glm::mat4 worldMatrix = rotationMatrix * translationMatrix * scalingMatrix;
+            //TODO: needs to apply rotation relative to center of model
+            //Scale first, place all cubes relative to center of model, rotate them on center, then move out to proper place
+            glm::mat4 worldMatrix = translationMatrix * rotationMatrix * positionMatrix * scalingMatrix;
             glUniformMatrix4fv(worldLoc, 1, GL_FALSE, &worldMatrix[0][0]);
             glDrawArrays(GL_TRIANGLES, startIndex, NUM_CUBE_VERTICES); // 3 vertices, starting at index 0
         }
