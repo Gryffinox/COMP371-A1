6042bef827f8f9ed86cba79688fde44ce7f979a4
Archie27376 <72292738+Archie27376@users.noreply.github.com>
2021-07-19 20:01:40 -0400

    Dante Model Positions

---
+++ COMP371-A1/Dante.cpp
@@ -0,0 +1,11 @@
+glm::vec3 points[] = {
+      glm::vec3( 0.0f,  0.0f, 0.0f),
+      glm::vec3( -0.5f,  0.5f, 0.5f),
+      glm::vec3( -1.0f,  0.5f, 0.0f),
+      glm::vec3( 0.5f,  0.5f, -0.5f),
+      glm::vec3( 1.0f,  0.5f, 0.0f),
+      glm::vec3( -0.5f,  -0.5f, 0.5f),
+      glm::vec3( -1.0f,  -0.5f, 0.0f),
+      glm::vec3( 0.5f,  -0.5f, -0.5f),
+      glm::vec3( 1.0f,  -0.5f, 0.0f)
+ };
