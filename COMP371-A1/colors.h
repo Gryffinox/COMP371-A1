988ba1b3c54d1be1320df83cb09ca8cb35afcd52
amanda6mard <61531228+amanda6mard@users.noreply.github.com>
2021-07-24 18:32:19 -0400

    added colors

--- COMP371-A1/colors.h
+++ COMP371-A1/colors.h
@@ -1,4 +1,5 @@
-//  COMP 371 Assignment 1 team Last Minute Formed
+// COMP 371 Assignment 1 team Last Minute Formed
+// some colors selected using coolors.co
 
 #ifndef colors_h
 #define colors_h
@@ -17,6 +18,13 @@ const glm::vec3 DARK_ORANGE = rgb(231,111,81);
 const glm::vec3 TEAL = rgb(42, 157, 143);
 const glm::vec3 LIGHT_GREEN = rgb(138,177,125);
 const glm::vec3 FUSCHIA = rgb(255, 0, 128);
+const glm::vec3 JET = rgb(52,52,52);
+const glm::vec3 SPACE_CADET = rgb(50,50,75);
+const glm::vec3 LIGHT_SPACE_CADET = rgb(47,48,97);
+const glm::vec3 ST_PATS_BLUE = rgb(31,50,129);
+const glm::vec3 EGYPT_BLUE = rgb(14,52,160);
+const glm::vec3 PURPLE_NAVY = rgb(95,89,128);
+const glm::vec3 MANATEE = rgb(159,156,176); //light purple
 
 
 glm::vec3 rgb(float r, float g, float b)
