f77ff403583db19b9f3e43a6231ef71e96405149
amanda6mard <61531228+amanda6mard@users.noreply.github.com>
2021-07-30 16:24:32 -0400

    added shader files and images

--- COMP371-A1/FragmentShader.glsl
+++ COMP371-A1/FragmentShader.glsl
@@ -4,10 +4,20 @@ out vec4 FragColor;
 in vec3 vertexColor;
 in vec3 fragPos;
 in vec3 normal;
+in vec3 textureCoords;
 
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform vec3 lightColor = vec3(1.0,1.0,1.0);
+
+uniform float shininess = 32;
+
+uniform sampler2D tex;
+uniform bool textureOn;
+uniform sampler2D emissionMap;
+uniform bool glowOn;
+uniform float intensity = 1;
+uniform bool colorOn = true;
 
 void main()
 {
@@ -25,9 +35,20 @@ void main()
         float specularStrength = 0.5;
         vec3 viewDir = normalize(viewPos - fragPos);
         vec3 reflectDir = reflect(-lightDir, norm);
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
         vec3 specular = specularStrength * spec * lightColor;
-            
-        vec3 litColor = (ambient + diffuse + specular) * vertexColor;
+    
+        vec3 litColor = (ambient + diffuse + specular ) * vertexColor;
+        
+    if(textureOn)
+    {
+        FragColor = texture(tex, vec2(textureCoords.x, textureCoords.y)) *  vec4(litColor, 1.0);
+        if(glowOn)
+        {
+            FragColor += intensity * (texture(emissionMap, vec2(textureCoords.x, textureCoords.y)) *  vec4(vertexColor, 1.0));
+        }
+    } else {
         FragColor = vec4(litColor, 1.0);
+    }
+        
 }
