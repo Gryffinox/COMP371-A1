f61c671b249e04c1e3a656cfdf2dc6254c6831fc
Calvin <calvinkylee78@gmail.com>
2021-08-02 15:26:29 -0400

    Changed all the "Assignment" names to "Project"
    Changed solution name and window name and a comment. nuts i know

--- COMP371-A1/main.cpp
+++ COMP371-A1/main.cpp
@@ -1,6 +1,5 @@
 //
 // COMP 371 PrOJeCt asSiGnMenT by team LastMinuteFormed
-// COMP 371 Assignment 2 team LastMinuteFormed
 //
 // Built on in-class Lab Framework
 // And built on ressources from https://learnopengl.com
@@ -1064,7 +1063,7 @@ int main(int argc, char*argv[])
     
     // Create Window and rendering context using GLFW
     //Resolution 1024 x 768
-    GLFWwindow* window = glfwCreateWindow(DEFAULT_SCR_WIDTH, DEFAULT_SCR_HEIGHT, "Comp371 - Assignment", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(DEFAULT_SCR_WIDTH, DEFAULT_SCR_HEIGHT, "Comp371 Project", NULL, NULL);
     if (window == NULL)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
