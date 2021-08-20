169c2b5be042e2e6dc1f067a95ad730c584b3929
amanda6mard <61531228+amanda6mard@users.noreply.github.com>
2021-08-19 22:29:45 -0400

    mess oh files trying to get freetype to work

--- COMP371-A1/main.cpp
+++ COMP371-A1/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <map>
 
 
 #define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
@@ -18,6 +19,9 @@
 #include <glm/gtc/matrix_transform.hpp> // include this to create transformation matrices
 
 #include <irrKlang.h> // sound
+
+#include <ft2build.h> //text rendering
+#include FT_FREETYPE_H
 
 //Classes
 #include "shader.h"
@@ -37,6 +41,7 @@ void getInput(GLFWwindow* window, float deltaTime);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void createShadowDepthMap(GLuint& depthMapFBO, GLuint& depthMap);
 void playSound(char* filename, bool repeat);
+void renderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color);
 
 /*================================================================
 	Globals
@@ -65,6 +70,7 @@ Camera camera;
 Shader shader;
 Shader lightShader;
 Shader depthShader;
+Shader textShader;
 
 //Uniform variables
 bool drawShadows;
@@ -91,7 +97,21 @@ float rotationAnimationTime[3];
 ================================================================*/
 #include "game.h"
 
+
+//sound engine
 irrklang::ISoundEngine *SoundEngine = irrklang::createIrrKlangDevice();
+
+//text rendering
+struct Character {
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2   Size;      // Size of glyph
+    glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Horizontal offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
+
+unsigned int VAOT, VBOT;
 
 /*================================================================
 	Main
@@ -113,6 +133,9 @@ int main(int argc, char* argv[]) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #endif
+    
+        
+    
 	// Create Window and rendering context using GLFW
 	//Resolution 1024 x 768
 	GLFWwindow* window = glfwCreateWindow(DEFAULT_SCR_WIDTH, DEFAULT_SCR_HEIGHT, "Comp371 Project", NULL, NULL);
@@ -132,8 +155,84 @@ int main(int argc, char* argv[]) {
 		glfwTerminate();
 		return -1;
 	}
+    
+    //load font
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    FT_Face face;
+    if (FT_New_Face(ft, "Roboto-Regular.ttf", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+    
+    FT_Set_Pixel_Sizes(face, 0, 48);
+      
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // generate texture
+        unsigned int textTexture;
+        glGenTextures(1, &textTexture);
+        glBindTexture(GL_TEXTURE_2D, textTexture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            textTexture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            static_cast<unsigned int>(face->glyph->advance.x)
+        };
+        Characters.insert(std::pair<char, Character>(c, character));
+    
+    }
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    glGenVertexArrays(1, &VAOT);
+    glGenBuffers(1, &VBOT);
+    glBindVertexArray(VAOT);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOT);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    
 	//hide mouse
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	// Set Background Color
 	glClearColor(DARK_BLUE.x, DARK_BLUE.y, DARK_BLUE.z, 1.0f);
 	// Enable Depth Test
@@ -148,6 +247,12 @@ int main(int argc, char* argv[]) {
 	shader = Shader("VertexShader.glsl", "FragmentShader.glsl");
 	lightShader = Shader("VertexShaderLight.glsl", "FragmentShaderLight.glsl");
 	depthShader = Shader("VertexShaderDepth.glsl", "FragmentShaderDepth.glsl");
+    textShader = Shader("VertexShaderText.glsl", "FragmentShaderText.glsl");
+    
+    textShader.use();
+    glm::mat4 textprojection = glm::ortho(0.0f, screenWidth, 0.0f, screenHeight);
+    textShader.setMat4("projection", textprojection);
+    textShader.setVec3("textColor", glm::vec3(0,0,0));
 
 	//Light position
 	glm::vec3 lightPos = glm::vec3(20.f, 20.f, -10.f);
@@ -264,6 +369,9 @@ int main(int argc, char* argv[]) {
 		shader.use();
 		glBindVertexArray(whiteCubeVAO);
 		drawModel(shader);
+        
+        //textShader.use();
+        renderText(textShader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
 
 		// render Depth map to quad for visual debugging
 		// ---------------------------------------------
@@ -600,3 +708,46 @@ void playSound(char* filename, bool repeat)
     SoundEngine->play2D(filename, repeat);
 }
 
+void renderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    // activate corresponding render state
+    s.use();
+    glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAOT);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBOT);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
